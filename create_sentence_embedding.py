826c829b2e921e8180117f83d2de31aa40471980
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2017-10-16 22:31:31 +0530

    Couple of changes in 3.1

--- create_sentence_embedding.py
+++ create_sentence_embedding.py
@@ -1,30 +1,29 @@
 from downsample import *
 import numpy as np
 import pickle
+from nltk import sent_tokenize
 import re
 import skipthoughts
 
 model = skipthoughts.load_model()
 encoder = skipthoughts.Encoder(model)
-document_embeddings = []
-labels = []
+# document_embeddings = []
+document_embeddings = open('document_embeddings_from_skipthoughts', 'w')
 
 for directory in data_label :
     files = os.listdir(DATA_DIR+directory)
     for f in files :
         file_descriptor = open(DATA_DIR+directory+"/"+f)
         file_content = file_descriptor.read()
-        file_content = file_content.split('.')
-        for sentence in file_content:
-            sentence = re.split(r'[^a-zA-Z0-9]', sentence) # Check if it works
-
+        file_content = sent_tokenize(file_content)
         vectors = encoder.encode(file_content) # It will give embedding of all the sentences of the document
         document_embedding = np.average(vectors, axis=0)
-        document_embeddings.append(document_embedding)
-        labels.append(label_to_id[directory])
+        s = ""
+        for doc in document_embedding :
+            s += str(doc) + ","
+        s = s[:-1]
+        document_embeddings.write(s)
+        # document_embeddings.append(document_embedding)
 
-
-document_embeddings_file = open('document_embeddings_from_skipthoughts.pickle', 'w')
-labels_file = open('labels_from_skipthoughts.pickle', 'w')
-pickle.dump(document_embeddings, document_embeddings_file)
-pickle.dump(labels, labels_file)
+# document_embeddings_file = open('document_embeddings_from_skipthoughts.pickle', 'w')
+# pickle.dump(document_embeddings, document_embeddings_file)
